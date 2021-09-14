39788aac4d13e1d88f674fe9c2a336a0ab5bdee0
christofjens <christofjens@gmail.com>
2021-09-14 23:43:53 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Button from './Components/Button'
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div>
+        <Button primaryGreen small>
+          primary green S
+        </Button>
+        <Button secondaryGreen small>
+          secondary green S
+        </Button>
+        <Button primaryGrey small>
+          primary grey S
+        </Button>
+        <Button secondaryGrey small>
+          secondary grey S
+        </Button>
+      </div>
+      <div>
+        <Button primaryGreen medium>
+          primary green M
+        </Button>
+        <Button secondaryGreen medium>
+          secondary green M
+        </Button>
+        <Button primaryGrey medium>
+          primary grey M
+        </Button>
+        <Button secondaryGrey medium>
+          secondary grey M
+        </Button>
+      </div>
+      <div>
+        <Button primaryGreen large>
+          primary green L
+        </Button>
+        <Button secondaryGreen large>
+          secondary green L
+        </Button>
+        <Button primaryGrey large>
+          primary grey L
+        </Button>
+        <Button secondaryGrey large>
+          secondary grey L
+        </Button>
+      </div>
     </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/Components/Button.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+export default function Button(props) {
+  return <ButtonStyled {...props} />
+}
+
+const ButtonStyled = styled.button`
+  color: ${props => (props.primaryGreen ? 'white' : props.secondaryGreen ? 'green' : props.primaryGrey ? 'white' : props.secondaryGrey ? 'lightgrey' : 'grey')};
+  margin: 20px 10px;
+  padding: ${props => (props.large ? '20px 32px' : props.medium ? '16px 24px' : '12px 16px')};
+  border-radius: ${props => (props.large ? '16px' : props.medium ? '12px' : props.small ? '8px' : '0')};
+  border: ${props => (props.primaryGreen ? '2px solid green' : props.secondaryGreen ? '2px solid green' : props.primaryGrey ? '2px solid lightgrey' : props.secondaryGrey ? '2px solid lightgrey' : 'grey')};
+  background-color: ${props => (props.primaryGreen ? 'green' : props.secondaryGreen ? 'white' : props.primaryGrey ? 'lightgrey' : props.secondaryGrey ? 'white' : 'grey')};
+  font-weight: 900;
+  font-size: ${props => (props.large ? '1.4rem' : props.medium ? '1.2rem' : '1rem')};
+
+  &:hover {
+    box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.25);
+    background-color: ${props => (props.primaryGreen ? 'darkgreen' : props.secondaryGreen ? 'white' : props.primaryGrey ? 'dimgrey' : props.secondaryGrey ? 'white' : 'grey')};
+    border: ${props => (props.primaryGreen ? '2px solid darkgreen' : props.secondaryGreen ? '2px solid darkgreen' : props.primaryGrey ? '2px solid dimgrey' : props.secondaryGrey ? '2px solid dimgrey' : 'grey')};
+    color: ${props => (props.primaryGreen ? 'white' : props.secondaryGreen ? 'darkgreen' : props.primaryGrey ? 'white' : props.secondaryGrey ? 'dimgrey' : 'grey')};
+  }
+`
