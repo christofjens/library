0af4fed5c05bc9c39ae19654b5570b2d4aaadc76
christofjens <christofjens@gmail.com>
2021-09-16 10:27:06 +0200

    Add transition

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -5,6 +5,7 @@ export default function Button(props) {
 }
 
 const ButtonStyled = styled.button`
+  transition: 0.2s ease-in-out;
   color: ${props => (props.primaryGreen ? 'white' : props.secondaryGreen ? 'green' : props.primaryGrey ? 'white' : props.secondaryGrey ? 'lightgrey' : 'grey')};
   margin: 20px 10px;
   padding: ${props => (props.large ? '20px 32px' : props.medium ? '16px 24px' : '12px 16px')};
@@ -15,6 +16,7 @@ const ButtonStyled = styled.button`
   font-size: ${props => (props.large ? '1.4rem' : props.medium ? '1.2rem' : '1rem')};
 
   &:hover {
+    transition: 0.2s ease-in-out;
     box-shadow: 2px 2px 5px 2px rgba(0, 0, 0, 0.25);
     background-color: ${props => (props.primaryGreen ? 'darkgreen' : props.secondaryGreen ? 'white' : props.primaryGrey ? 'dimgrey' : props.secondaryGrey ? 'white' : 'grey')};
     border: ${props => (props.primaryGreen ? '2px solid darkgreen' : props.secondaryGreen ? '2px solid darkgreen' : props.primaryGrey ? '2px solid dimgrey' : props.secondaryGrey ? '2px solid dimgrey' : 'grey')};
